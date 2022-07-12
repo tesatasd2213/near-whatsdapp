67627bff0464a76cb1e1ecdc267fb44ecaa6ca2d
piontkovsk11andre1 <andrei@p10i.ru>
2022-07-12 07:05:56 +0000

    update package and contract names

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || "whatsdapp.testnet";
+const CONTRACT_NAME = process.env.CONTRACT_NAME || "testaccount19239129312.testnet";
 
 function getConfig(env) {
   switch (env) {
